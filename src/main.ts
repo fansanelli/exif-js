d37f066baa492ebb68d202b83b48d6ad99fe755d
Francesco Ansanelli <francians@gmail.com>
2024-06-01 18:21:29 +0200

    typescript conversion. untested and not reviewed

---
+++ src/main.ts
@@ -0,0 +1,84 @@
+'use strict';
+
+import * as exif from './impl/exif';
+
+export function enableXmp(): void {
+  exif.enableXmp();
+}
+
+export function disableXmp(): void {
+  exif.disableXmp();
+}
+
+export function getData(img: any, callback: any): any {
+  if (((self.Image && img instanceof self.Image)
+    || (self.HTMLImageElement && img instanceof self.HTMLImageElement))
+    && !img.complete)
+    return false;
+  if (!exif.imageHasData(img)) {
+    exif.getImageData(img, callback);
+  } else {
+    if (callback) {
+      callback.call(img);
+    }
+  }
+  return true;
+}
+
+export function getTag(img: any, tag: any): any {
+  if (!exif.imageHasData(img)) return;
+  return img.exifdata[tag];
+}
+
+export function getIptcTag(img: any, tag: any): any {
+  if (!exif.imageHasData(img)) return;
+  return img.iptcdata[tag];
+}
+
+export function getAllTags(img: any): any {
+  if (!exif.imageHasData(img)) return {};
+  const tags: { [tag: string]: any } = {};
+  const data = img.exifdata;
+  for (const a in data) {
+    if (data.hasOwnProperty(a)) {
+      tags[a] = data[a];
+    }
+  }
+  return tags;
+}
+
+export function getAllIptcTags(img: any): any {
+  if (!exif.imageHasData(img)) return {};
+  const tags: { [tag: string]: any } = {};
+  const data = img.iptcdata;
+  for (const a in data) {
+    if (data.hasOwnProperty(a)) {
+      tags[a] = data[a];
+    }
+  }
+  return tags;
+}
+
+export function pretty(img: any): string {
+  if (!exif.imageHasData(img)) return "";
+  let strPretty = "";
+  const data = img.exifdata;
+  for (const a in data) {
+    if (data.hasOwnProperty(a)) {
+      if (typeof data[a] == "object") {
+        if (data[a] instanceof Number) {
+          strPretty += a + " : " + data[a] + " [" + data[a].numerator + "/" + data[a].denominator + "]\r\n";
+        } else {
+          strPretty += a + " : [" + data[a].length + " values]\r\n";
+        }
+      } else {
+        strPretty += a + " : " + data[a] + "\r\n";
+      }
+    }
+  }
+  return strPretty;
+}
+
+export function readFromBinaryFile(file: any): any {
+  return exif.findEXIFinJPEG(file);
+}
